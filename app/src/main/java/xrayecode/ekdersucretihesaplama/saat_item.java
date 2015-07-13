56dd145cd7610753e5f6f6a77473dafc1421d82a
xrayecode <xrayecode@gmail.com>
2015-07-14 02:04:41 +0300

    Initial commit

---
+++ app/src/main/java/xrayecode/ekdersucretihesaplama/saat_item.java
@@ -0,0 +1,151 @@
+package xrayecode.ekdersucretihesaplama;
+
+/**
+ * Created by akkaraman on 13.7.2015.
+ */
+public class saat_item {
+    private String unvan;
+    private String egitim_turu;
+    private String son_ogrenim;
+    private String vergi_dilimi;
+    private boolean layout_1;
+    private boolean layout_2;
+    private boolean layout_3;
+    private boolean layout_4;
+    private boolean layout_5;
+    private boolean layout_6;
+    private String label_1;
+    private String label_2;
+    private String label_3;
+    private String label_4;
+    private String label_5;
+    private String label_6;
+
+    public String getUnvan() {
+        return unvan;
+    }
+
+    public void setUnvan(String unvan) {
+        this.unvan = unvan;
+    }
+
+    public String getEgitim_turu() {
+        return egitim_turu;
+    }
+
+    public void setEgitim_turu(String egitim_turu) {
+        this.egitim_turu = egitim_turu;
+    }
+
+    public String getSon_ogrenim() {
+        return son_ogrenim;
+    }
+
+    public void setSon_ogrenim(String son_ogrenim) {
+        this.son_ogrenim = son_ogrenim;
+    }
+
+    public String getVergi_dilimi() {
+        return vergi_dilimi;
+    }
+
+    public void setVergi_dilimi(String vergi_dilimi) {
+        this.vergi_dilimi = vergi_dilimi;
+    }
+
+    public boolean isLayout_1() {
+        return layout_1;
+    }
+
+    public void setLayout_1(boolean layout_1) {
+        this.layout_1 = layout_1;
+    }
+
+    public boolean isLayout_2() {
+        return layout_2;
+    }
+
+    public void setLayout_2(boolean layout_2) {
+        this.layout_2 = layout_2;
+    }
+
+    public boolean isLayout_3() {
+        return layout_3;
+    }
+
+    public void setLayout_3(boolean layout_3) {
+        this.layout_3 = layout_3;
+    }
+
+    public boolean isLayout_4() {
+        return layout_4;
+    }
+
+    public void setLayout_4(boolean layout_4) {
+        this.layout_4 = layout_4;
+    }
+
+    public boolean isLayout_5() {
+        return layout_5;
+    }
+
+    public void setLayout_5(boolean layout_5) {
+        this.layout_5 = layout_5;
+    }
+
+    public boolean isLayout_6() {
+        return layout_6;
+    }
+
+    public void setLayout_6(boolean layout_6) {
+        this.layout_6 = layout_6;
+    }
+
+    public String getLabel_1() {
+        return label_1;
+    }
+
+    public void setLabel_1(String label_1) {
+        this.label_1 = label_1;
+    }
+
+    public String getLabel_2() {
+        return label_2;
+    }
+
+    public void setLabel_2(String label_2) {
+        this.label_2 = label_2;
+    }
+
+    public String getLabel_3() {
+        return label_3;
+    }
+
+    public void setLabel_3(String label_3) {
+        this.label_3 = label_3;
+    }
+
+    public String getLabel_4() {
+        return label_4;
+    }
+
+    public void setLabel_4(String label_4) {
+        this.label_4 = label_4;
+    }
+
+    public String getLabel_5() {
+        return label_5;
+    }
+
+    public void setLabel_5(String label_5) {
+        this.label_5 = label_5;
+    }
+
+    public String getLabel_6() {
+        return label_6;
+    }
+
+    public void setLabel_6(String label_6) {
+        this.label_6 = label_6;
+    }
+}
